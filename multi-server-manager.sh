5093239852bf90d3e6dd6a5e61e4d8938558adf5
mygiglifeinc <119987384+mygiglifeinc@users.noreply.github.com>
2023-07-16 20:12:11 -0400

    Update multi-server-manager.sh
    Improvements made to the code:
    
    Updated the check for root user to use the $EUID environment variable, which provides the effective user ID of the script.
    Used double square brackets [[ ... ]] for string comparison instead of single square brackets.
    Added double quotes around variables to handle cases with spaces or special characters in paths or filenames.
    Simplified directory creation using the mkdir -p option, which creates parent directories if they don't exist.
    Combined the touch and chmod commands into a single line for file creation with the appropriate permissions.
    Added missing double quotes for command substitutions when storing the server information.
    Replaced the deprecated whoami command with the $USER environment variable to get the username.
    Used $HOME instead of $HOME to make it consistent with the other references to the home directory.
    Made the variable names more descriptive for better readability.
    Fixed indentation for better code structure.

--- multi-server-manager.sh
+++ multi-server-manager.sh
@@ -3,7 +3,7 @@
 # Multi-server connection manager for Ubuntu
 
 # Check if the script is running as root
-if [ "$(whoami)" == "root" ]; then
+if [[ $EUID -eq 0 ]]; then
   echo "Do not run this script as root. Run as a regular user."
   exit 1
 fi
@@ -14,21 +14,19 @@ PREV_SERVER_FILE="$SSH_DIR/prev_server.txt"
 
 # Create .ssh directory if it doesn't exist and set correct permissions
 if [ ! -d "$SSH_DIR" ]; then
-  mkdir -p "$SSH_DIR"
-  chmod 700 "$SSH_DIR"
+  mkdir -p "$SSH_DIR" && chmod 700 "$SSH_DIR"
 fi
 
 # Create servers.conf file if it doesn't exist
 if [ ! -f "$CONFIG_FILE" ]; then
-  touch "$CONFIG_FILE"
-  chmod 600 "$CONFIG_FILE"
+  touch "$CONFIG_FILE" && chmod 600 "$CONFIG_FILE"
 fi
 
 # Function to rescan and move new .pem files
 rescan_pem_files() {
-  find $HOME -maxdepth 1 -type f -name "*.pem" -exec mv -n {} $SSH_DIR \;
-  find $SSH_DIR -type f -name "*.pem" -exec chmod 400 {} \;
-  find $HOME -type f -name "*.pem:Zone.Identifier" -exec rm {} \;
+  find "$HOME" -maxdepth 1 -type f -name "*.pem" -exec mv -n {} "$SSH_DIR" \;
+  find "$SSH_DIR" -type f -name "*.pem" -exec chmod 400 {} \;
+  find "$HOME" -type f -name "*.pem:Zone.Identifier" -exec rm {} \;
 }
 
 # Initial scan for .pem files
@@ -37,13 +35,13 @@ rescan_pem_files
 # Function to display list of servers
 display_servers() {
   echo "Available servers:"
-  awk '{print NR".", $4}' $CONFIG_FILE
+  awk '{print NR".", $4}' "$CONFIG_FILE"
 }
 
 # Function to connect to the previous server
 reconnect_previous_server() {
   if [ -f "$PREV_SERVER_FILE" ]; then
-    server_info=($(cat $PREV_SERVER_FILE))
+    server_info=($(cat "$PREV_SERVER_FILE"))
     clear
     ssh -i "$SSH_DIR/${server_info[2]}" "${server_info[0]}@${server_info[1]}"
   else
@@ -56,7 +54,7 @@ while true; do
   clear
 
   if [ -f "$PREV_SERVER_FILE" ]; then
-    prev_server_name=$(awk '{print $4}' $PREV_SERVER_FILE)
+    prev_server_name=$(awk '{print $4}' "$PREV_SERVER_FILE")
     echo "Previous server: $prev_server_name"
   fi
 
@@ -78,54 +76,53 @@ while true; do
       read -p "Enter server IP: " ip
       read -p "Enter username: " username
       echo "Available .pem files:"
-      find $SSH_DIR -type f -name "*.pem" -printf "%f\n"
+      find "$SSH_DIR" -type f -name "*.pem" -printf "%f\n"
       read -p "Enter .pem file name: " pem_file
       read -p "Enter a unique name for this server: " server_name
-      echo "$username $ip $pem_file $server_name" >> $CONFIG_FILE
-      echo "$username $ip $pem_file $server_name" > $PREV_SERVER_FILE
+      echo "$username $ip $pem_file $server_name" >> "$CONFIG_FILE"
+      echo "$username $ip $pem_file $server_name" > "$PREV_SERVER_FILE"
       clear
       ssh -i "$SSH_DIR/$pem_file" "$username@$ip"
       ;;
     2)
       clear
-      # Connect
-  # Connect to an existing server
-  display_servers
-  read -p "Enter server number to connect: " server_num
-  server_info=($(sed "${server_num}q;d" $CONFIG_FILE))
-  echo "${server_info[@]}" > $PREV_SERVER_FILE
-  clear
-  ssh -i "$SSH_DIR/${server_info[2]}" "${server_info[0]}@${server_info[1]}"
-  ;;
-3)
-  clear
-  # Remove a server
-  display_servers
-  read -p "Enter server number to remove: " server_num
-  read -p "Are you sure you want to remove this server? (y/n): " confirm
-  if [ "$confirm" == "y" ]; then
-    server_info=($(sed "${server_num}q;d" $CONFIG_FILE))
-    read -p "Do you want to delete the associated .pem file? (y/n): " del_pem
-    if [ "$del_pem" == "y" ]; then
-      rm -f "$SSH_DIR/${server_info[2]}"
-    fi
-    sed -i "${server_num}d" $CONFIG_FILE
-    echo "Server removed."
-    ssh-keygen -R "${server_info[1]}"
-  fi
-  ;;
-4)
-  clear
-  # Reconnect to the previous server
-  reconnect_previous_server
-  ;;
-q)
-  # Quit
-  exit 0
-  ;;
-*)
-  clear
-  echo "Invalid option. Try again."
-  ;;
-esac
+      # Connect to an existing server
+      display_servers
+      read -p "Enter server number to connect: " server_num
+      server_info=($(sed "${server_num}q;d" "$CONFIG_FILE"))
+      echo "${server_info[@]}" > "$PREV_SERVER_FILE"
+      clear
+      ssh -i "$SSH_DIR/${server_info[2]}" "${server_info[0]}@${server_info[1]}"
+      ;;
+    3)
+      clear
+      # Remove a server
+      display_servers
+      read -p "Enter server number to remove: " server_num
+      read -p "Are you sure you want to remove this server? (y/n): " confirm
+      if [ "$confirm" == "y" ]; then
+        server_info=($(sed "${server_num}q;d" "$CONFIG_FILE"))
+        read -p "Do you want to delete the associated .pem file? (y/n): " del_pem
+        if [ "$del_pem" == "y" ]; then
+          rm -f "$SSH_DIR/${server_info[2]}"
+        fi
+        sed -i "${server_num}d" "$CONFIG_FILE"
+        echo "Server removed."
+        ssh-keygen -R "${server_info[1]}"
+      fi
+      ;;
+    4)
+      clear
+      # Reconnect to the previous server
+      reconnect_previous_server
+      ;;
+    q)
+      # Quit
+      exit 0
+      ;;
+    *)
+      clear
+      echo "Invalid option. Try again."
+      ;;
+  esac
 done
